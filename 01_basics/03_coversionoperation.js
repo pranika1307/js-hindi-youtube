6d2f497e1b966d3fe5c31bba0c3c980157997e4c
Pranika seth <sethpakhi20@gmail.com>
2025-04-22 05:38:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionoperation.js
@@ -0,0 +1,12 @@
+let score ="33acc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+

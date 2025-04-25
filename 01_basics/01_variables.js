1a6e8d8f03cec64da1e7011b641e16f40752c8ce
Pranika seth <sethpakhi20@gmail.com>
2025-04-25 18:38:35 +0000

    datatype-summary

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail= "pranikaseth@gmail.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountState;
+
+accountEmail ="hc@hc.com"
+accountPassword ="3748748"
+accountCity ="Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ datatypes-summary.js
@@ -0,0 +1,8 @@
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId =Symbol('123')
+console.log(id==anotherId);
